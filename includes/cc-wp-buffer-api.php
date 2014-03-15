4c0dd1d343f2ace6fbd3c7c1f57cc682c894c4a7
cconover <dev@cconover.com>
2014-03-15 22:00:54 +0000

    Added methods and properties for class initialization

--- includes/cc-wp-buffer-api.php
+++ includes/cc-wp-buffer-api.php
@@ -7,10 +7,27 @@
 namespace cconover\buffer;
 
 class Api extends Buffer {
+	/* Class properties */
+	protected $clientconfig; // OAuth client configuration
+	
 	// Class constructor
 	function __construct() {
 		// Initialize
 		$this->initialize();
+		$this->initialize_api();
 	} // End __construct()
+	
+	/* API initialization and configuration */
+	// Initialize the API
+	function initialize_api() {
+		// Load the OAuth client configuration
+		$this->clientconfig();
+	} // End initialize_api()
+	
+	// OAuth client configuration
+	function clientconfig() {
+		
+	} // End clientconfig()
+	/* End API initialization and configuration */
 }
 ?>
