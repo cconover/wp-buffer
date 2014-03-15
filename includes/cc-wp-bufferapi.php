cca58c09a049ae0ce6d330aa1c894a58d4862500
cconover <dev@cconover.com>
2014-03-15 05:19:22 +0000

    Created includes directory inside plugin root. Contains the file for the Buffer API class.

---
+++ includes/cc-wp-bufferapi.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Buffer API class
+ * Handles all API calls for the plugin
+ **/
+
+class cc_wp_bufferapi extends cc_wp_buffer {
+	// Class constructor
+	function __construct() {
+		// Initialize
+		$this->initialize();
+	} // End __construct()
+}
+?>
