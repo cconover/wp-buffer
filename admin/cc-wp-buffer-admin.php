b5cd40fcdaea35aa9ae5acf3eb8fb204142bfda9
cconover <dev@cconover.com>
2014-03-19 15:09:38 +0000

    Wrote better validation calls for variable values

--- admin/cc-wp-buffer-admin.php
+++ admin/cc-wp-buffer-admin.php
@@ -4,6 +4,7 @@
  * admin/cc-wp-buffer-admin.php
  **/
 
+// Plugin namespace
 namespace cconover\buffer;
 
 /**
@@ -15,6 +16,7 @@ class Admin extends Buffer {
 	function __construct() {
 		// Initialize
 		$this->initialize();
+		$this->api_initialize();
 		
 		/* Hooks and filters */
 		add_action( 'admin_menu', array( &$this, 'create_options_menu' ) ); // Add menu entry to Settings menu
@@ -73,7 +75,7 @@ class Admin extends Buffer {
 		$this->auth_options();
 		
 		// If the application has been fully authenticated with Buffer, load the rest of the options
-		if ( $this->options['client_id'] && $this->options['client_secret'] && $this->options['access_token'] ) {
+		if ( ! empty( $this->options['client_id'] ) && ! empty( $this->options['client_secret'] ) && ! empty( $this->options['site_access_token'] ) ) {
 			// Options page sections and fields
 			$this->set_options_sections();
 			$this->set_options_fields();
@@ -89,31 +91,33 @@ class Admin extends Buffer {
 			array( &$this, 'auth_callback' ), // Callback method to display plugin options
 			self::ID // Page ID for the options page
 		);
-		
-		// Buffer application client ID
-		add_settings_field(
-			'client_id', // Field ID
-			'Client ID', // Field title/label, displayed to the user
-			array( &$this, 'client_id_callback' ), // Callback method to display the option field
-			self::ID, // Page ID for the options page
-			'auth' // Settings section in which to display the field
-		);
-		
-		// Buffer application client secret
-		add_settings_field(
-			'client_secret', // Field ID
-			'Client secret', // Field title/label, displayed to the user
-			array( &$this, 'client_secret_callback' ), // Callback method to display the option field
-			self::ID, // Page ID for the options page
-			'auth' // Settings section in which to display the field
-		);
+		// If the Client ID and Client Secret are not stored in the database, show the fields for those items
+		if ( empty( $this->options['client_id'] ) || empty( $this->options['client_secret'] ) ) {
+			// Buffer application client ID
+			add_settings_field(
+				'client_id', // Field ID
+				'Client ID', // Field title/label, displayed to the user
+				array( &$this, 'client_id_callback' ), // Callback method to display the option field
+				self::ID, // Page ID for the options page
+				'auth' // Settings section in which to display the field
+			);
+			
+			// Buffer application client secret
+			add_settings_field(
+				'client_secret', // Field ID
+				'Client secret', // Field title/label, displayed to the user
+				array( &$this, 'client_secret_callback' ), // Callback method to display the option field
+				self::ID, // Page ID for the options page
+				'auth' // Settings section in which to display the field
+			);
+		}
 		
 		// Buffer access token to be used globally for the site (only show if Client ID and Client Secret are saved)
-		if ( $this->options['client_id'] && $this->options['client_secret'] ) {
+		if ( ! empty( $this->options['client_id'] ) && ! empty( $this->options['client_secret'] ) ) {
 			add_settings_field(
-				'access_token', // Field ID
+				'site_access_token', // Field ID
 				'Access Token', // Field title/label, displayed to the user
-				array( &$this, 'access_token_callback' ), // Callback method to display the option field
+				array( &$this, 'site_access_token_callback' ), // Callback method to display the option field
 				self::ID, // Page ID for the options page
 				'auth' // Settings section in which to display the field
 			);
@@ -217,16 +221,16 @@ class Admin extends Buffer {
 	// Authorization section
 	function auth_callback() {
 		// If client ID & secret haven't yet been saved, display this message
-		if ( ! $this->options['client_id'] || ! $this->options['client_secret'] ) {
-			echo '<p style="color: #E30000; font-weight: bold;">In order to use this plugin, you need to <a href="https://bufferapp.com/developers/apps/create" target="_blank">register it as a Buffer application</a></p><p>Don\'t worry, I\'ll walk you through it. Once you\'ve registered the application, copy the Client ID and Client Secret from the email you receive and paste them here.</p>';
+		if ( empty( $this->options['client_id'] ) || empty( $this->options['client_secret'] ) ) {
+			echo '<p style="color: #E30000; font-weight: bold;">In order to use this plugin, you need to <a href="https://bufferapp.com/developers/apps/create" target="_blank">register it as a Buffer application</a></p><p>Don\'t worry, I\'ll walk you through it. Once you\'ve registered the application, copy the Client ID and Client Secret from the email you receive and paste them here.</p><p><strong>Callback URL</strong>: NEEDS TO BE SET</p>';
 		}
 		// If they have been saved, check whether there's an access token and display the appropriate message
 		else {
-			if ( $this->options['access_token'] ) {
+			if ( ! empty( $this->options['site_access_token'] ) ) {
 				echo '<p style="color: #199E22;">This site is fully authenticated with Buffer. Have fun!</p>';
 			}
 			else {
-				echo '<p style="color: #F08C00;">You\'re almost done! Copy the access token for the <a href="https://bufferapp.com/developers/apps" target="_blank">application you just registered</a> and paste it in the Access Token field.</p>';
+				echo '<p style="color: #F08C00;">You\'re almost done! Copy the access token for the <a href="https://bufferapp.com/developers/apps" target="_blank">application you just registered</a> and paste it below.</p>';
 			}
 		}
 	} // End auth_callback()
@@ -256,7 +260,7 @@ class Admin extends Buffer {
 	// Client secret
 	function client_secret_callback() {
 		// If client secret is saved in the database, the field is type 'password'. If not, it's type 'text'.
-		if ( $this->options['client_secret'] ) {
+		if ( ! empty( $this->options['client_secret'] ) ) {
 			echo '<input type="password" name="' . $this->prefix . 'options[client_secret]" id="' . $this->prefix . 'options[client_secret]" value="' . $this->options['client_secret'] . '" size=40>';
 		}
 		else {
@@ -265,9 +269,9 @@ class Admin extends Buffer {
 	} // End client_id_callback()
 	
 	// Access token
-	function access_token_callback() {
+	function site_access_token_callback() {
 		// Show text input to provide access token
-		echo '<input type="text" name="' . $this->prefix . 'options[access_token]" id="' . $this->prefix . 'options[access_token]" value="' . $this->options['access_token'] . '" size=40>';
+		echo '<input type="text" name="' . $this->prefix . 'options[site_access_token]" id="' . $this->prefix . 'options[site_access_token]" value="' . $this->options['site_access_token'] . '" size=40>';
 	} // End client_id_callback()
 	
 	// Enable Twitter
@@ -298,7 +302,7 @@ class Admin extends Buffer {
 		
 		// Show the options fields with explanation
 		echo '<p>Send ' . $numposts . ' additional tweets, spaced ' . $interval . ' hours apart</p>';
-		echo '<em>A tweet will automatically be sent when a post is published. This setting lets you schedule follow-up tweets to be sent by Buffer.</em>';
+		echo '<span class="description">A tweet will automatically be sent when a post is published. This setting lets you schedule follow-up tweets to be sent by Buffer.</span>';
 	} // End twitter_schedule_callback()
 	
 	// Enable Facebook
@@ -344,7 +348,7 @@ class Admin extends Buffer {
 		$options = $this->options;
 		
 		// If client ID and client secret have been changed from what's in the database, validate them
-		if ( ( $input['client_id'] != $this->options['client_id'] ) || ( $input['client_secret'] != $this->options['client_secret'] ) ) {
+		if ( empty( $this->options['client_id'] ) || empty( $this->options['client_secret'] ) ) {
 			// Check to make sure whether the provided values are hexadecimal
 			if ( ctype_xdigit( $input['client_id'] ) && ctype_xdigit( $input['client_secret'] ) ) {
 				$options['client_id'] = $input['client_id']; // Application client ID
@@ -355,32 +359,46 @@ class Admin extends Buffer {
 				add_settings_error (
 					self::ID, // Setting to which the error applies
 					'client-auth', // Identify the option throwing the error
-					'The Client ID and/or Client Secret is in an invalid format. Please try again.', // Error message
+					'Whoops! The client ID or client secret you entered doesn\'t match Buffer\'s format. Double-check them both, and take another crack at it.', // Error message
 					'error' // The type of message it is
 				);
 			}
 		}
 		
 		// Access token will only be saved if Client ID and Client Secret are both already saved
-		if ( $this->options['client_id'] && $this->options['client_secret'] ) {
+		if ( ! empty( $this->options['client_id'] ) && ! empty( $this->options['client_secret'] ) ) {
 			// Make sure a value is provided for the access token
-			if ( $input['access_token'] ) {
-				// Save the access token
-				$options['access_token'] = $input['access_token'];
+			if ( ! empty( $input['site_access_token'] ) ) {
+				// Only perform the validation tasks if the value has changed from what's in the database
+				if ( $input['site_access_token'] != $this->options['site_access_token'] ) {
+					// Make sure the user is valid
+					if ( $this->api->validate_user( $input['site_access_token'] ) ) {
+						$options['site_access_token'] = $input['site_access_token'];
+					}
+					// If the user is not valid, throw an error
+					else {
+						add_settings_error (
+							self::ID, // Setting to which the error applies
+							'site-access-token', // Identify the option throwing the error
+							'Whoops! Buffer says that access token isn\'t quite right. Let\'s double-check what we put in, and give it another shot!', // Error message
+							'error' // The type of message it is
+						);
+					}
+				}
 			}
 			// If nothing is provided for the access token, throw an error
 			else {
 				add_settings_error (
 					self::ID, // Setting to which the error applies
-					'access-token', // Identify the option throwing the error
-					'The Buffer access token you provided is invalid.', // Error message
+					'site-access-token', // Identify the option throwing the error
+					'So close! It looks like you didn\'t provide an access token, and we can\'t continue without one. Let\'s try again!', // Error message
 					'error' // The type of message it is
 				);
 			}
 		}
 		
 		// All other options require application to be fully authenticated, and no validation or assigment will be done without that
-		if ( $this->options['client_id'] && $this->options['client_secret'] && $this->options['access_token'] ) {
+		if ( ! empty( $this->options['client_id'] ) && ! empty( $this->options['client_secret'] ) && ! empty( $this->options['site_access_token'] ) ) {
 			/* Twitter options */
 			// Check the value for enabling Twitter
 			if ( $input['twitter_send'] == 'yes' || $input['twitter_send'] == null ) {
@@ -399,7 +417,7 @@ class Admin extends Buffer {
 					add_settings_error (
 						self::ID, // Setting to which the error applies
 						'twitter-publish-syntax', // Identify the option throwing the error
-						'The tweet format you provided for Twitter is invalid.', // Error message
+						'Hang on a second. The Twitter syntax you provided doesn\'t look quite right. Make sure everything is entered properly, and try it again.', // Error message
 						'error' // The type of message it is
 					);
 				}
@@ -493,7 +511,7 @@ class Admin extends Buffer {
 		return $options;
 	} // End options_validate()
 	
-	// Validate message syntax
+	// Validate message syntax (NEEDS SYNTAX CHECK ADDED)
 	function validate_syntax( $input ) {
 		// Check that the service is enabled
 		if ( $input ) {
@@ -522,7 +540,7 @@ class Admin extends Buffer {
 	 	$options = array(
 	 		'client_id' => null, // Application client ID
 	 		'client_secret' => null, // Application client secret
-	 		'access_token' => null, // Access token, for the Buffer account used to publish posts globally
+	 		'site_access_token' => null, // Access token, for the Buffer account used to publish posts globally
 	 		'twitter_send' => null, // Don't enable Twitter by default
 	 		'twitter_publish_syntax' => 'New Post: {title} {url}', // Default syntax of Twitter messages
 	 		'twitter_post_number' => 0, // Number of tweets to schedule
