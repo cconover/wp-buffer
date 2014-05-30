705e38adde7b490941d198cabffec17292157579
cconover <dev@cconover.com>
2014-05-30 15:35:48 +0000

    Added support for jQuery UI tabs in the meta box

--- admin/assets/js/edit.js
+++ admin/assets/js/edit.js
@@ -6,4 +6,7 @@ JavaScript to use when editing a post or page
 jQuery( document ).ready( function( $ ) {
 	// Show the meta box form (not shown by default; requires JavaScript for it to be visible)
 	$( '#cc-wp-buffer-metabox-contents' ).show();
+	
+	// Apply jQueryUI tabs to the meta box
+	$( "#cc-wp-buffer-metabox-contents" ).tabs();
 } );
